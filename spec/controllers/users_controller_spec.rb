c1f3af34cb39c8e632b33d788457493eb7bcb95f
Kevin Lau <darealcaffeine@gmail.com>
2011-08-04 21:09:25 -0400

    finished signup ed page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,7 +35,7 @@ describe UsersController do
     end
   end 
  
-   describe "GET 'new'" do
+    describe "GET 'new'" do
 
     it "should be successful" do
       get :new
@@ -45,6 +45,26 @@ describe UsersController do
     it "should have the right title" do
       get :new
       response.should have_selector("title", :content => "Sign up")
+    end
+  
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+     get :new
+     response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+     get :new
+     response.should have_selector("input[name='user[password_confirmation]'][type='password']")
     end
   end
 
