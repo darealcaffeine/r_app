c9bd11087d04b0593ac51b7e70a0edebd7f34ee2
Kevin Lau <darealcaffeine@gmail.com>
2011-09-07 18:17:23 -0400

    sucks

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,6 @@ ActiveRecord::Schema.define(:version => 20110907005043) do
     t.boolean  "admin",              :default => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
 end
